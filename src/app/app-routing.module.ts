3961ad01546e50d4344f3993cef71e30d6ac8f41
Gregor Tokarev <dev@gregortokarev.com>
2022-07-20 06:48:43 +0300

    Fix routings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,13 @@
 
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { AppComponent } from "./app.component";
 
 const routes: Routes = [
+  {
+    path: "",
+    component: AppComponent,
+  },
   {
     path: "auth",
     loadChildren: () => import("./auth/auth.module").then(m => m.AuthModule),
