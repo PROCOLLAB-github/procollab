e41a322588880b3ef453908206e7cab216f1ea60
gregor <e@tokarev.work>
2022-08-07 16:47:19 +0300

    Add unauth redirect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 /** @format */
 
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { AuthService } from "./auth/services";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit(): void {
+    if (this.authService.getTokens() === null) {
+      this.router.navigateByUrl("/auth/login");
+    }
+  }
+}
