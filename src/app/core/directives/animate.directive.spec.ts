3bab753ae7863a33adea668a77b012e0f01852e1
Gregor Tokarev <dev@gregortokarev.com>
2022-09-04 08:03:27 +0300

    Remove animate directive

--- src/app/core/directives/animate.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AnimateDirective } from './animate.directive';
-
-describe('AnimateDirective', () => {
-  it('should create an instance', () => {
-    const directive = new AnimateDirective();
-    expect(directive).toBeTruthy();
-  });
-});
