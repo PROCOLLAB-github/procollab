eef3810c714c5410009d3126fb449ad9070a6e54
gregor <e@tokarev.work>
2022-09-14 11:33:58 +0300

    update

--- src/app/office/projects/projects.component.spec.ts
+++ src/app/office/projects/projects.component.spec.ts
@@ -5,6 +5,7 @@ import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { ProjectsComponent } from "./projects.component";
 import { RouterTestingModule } from "@angular/router/testing";
 import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { ReactiveFormsModule } from "@angular/forms";
 
 describe("ProjectsComponent", () => {
   let component: ProjectsComponent;
@@ -12,7 +13,7 @@ describe("ProjectsComponent", () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule, HttpClientTestingModule],
+      imports: [RouterTestingModule, HttpClientTestingModule, ReactiveFormsModule],
       declarations: [ProjectsComponent],
     }).compileComponents();
   });
