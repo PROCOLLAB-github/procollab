19fcb4251724cc22365b419b3432792c2c6786c3
gregor <e@tokarev.work>
2023-01-18 17:22:12 +0300

    CU-861m8e236 move menu to header

--- src/app/ui/directives/editor-submit-button.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { EditorSubmitButtonDirective } from './editor-submit-button.directive';
-
-describe('EditorSubmitButtonDirective', () => {
-  it('should create an instance', () => {
-    const directive = new EditorSubmitButtonDirective();
-    expect(directive).toBeTruthy();
-  });
-});
