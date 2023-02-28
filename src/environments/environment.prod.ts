024ab8f32e6dd8b07673ee49656d76f71906cb34
gregor <e@tokarev.work>
2023-02-28 09:10:05 +0300

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,4 +4,5 @@ export const environment = {
   production: true,
   sentryDns: "https://fc61f416df6044bab8c7e1afd55f4355@o1186023.ingest.sentry.io/6577563",
   apiUrl: "https://api.procollab.ru",
+  websocketUrl: "wss://api.procollab.ru/ws",
 };
