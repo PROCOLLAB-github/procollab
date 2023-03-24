062453de39c2bbe76997428c54214613ffeac31c
gregor <e@tokarev.work>
2023-03-24 15:48:45 +0300

    CU-861mj8a10 unit tests for search component

--- src/app/ui/components/search/search.component.spec.ts
+++ src/app/ui/components/search/search.component.spec.ts
@@ -1,24 +1,37 @@
-import { ComponentFixture, TestBed } from "@angular/core/testing";
+/** @format */
 
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { FormsModule } from "@angular/forms";
 import { SearchComponent } from "./search.component";
 
 describe("SearchComponent", () => {
   let component: SearchComponent;
   let fixture: ComponentFixture<SearchComponent>;
+  let onSwitchSearchSpy: jasmine.Spy;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [SearchComponent],
+      imports: [FormsModule],
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(SearchComponent);
     component = fixture.componentInstance;
+    onSwitchSearchSpy = spyOn(component, "onSwitchSearch").and.callThrough();
     fixture.detectChanges();
   });
 
   it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should not call onSwitchSearch when clicked and openable is false", () => {
+    component.openable = false;
+    const searchDiv = fixture.nativeElement.querySelector(".search__other");
+    searchDiv.dispatchEvent(new Event("click"));
+    fixture.detectChanges();
+    expect(onSwitchSearchSpy).not.toHaveBeenCalled();
   });
 });
