f93b21b7140f4402940bf90f7dd88b183c2bfc0f
gregor <e@tokarev.work>
2023-03-29 13:51:37 +0300

    CU-861mj8a10 fix count

--- src/app/core/interceptors/bearer-token.interceptor.ts
+++ src/app/core/interceptors/bearer-token.interceptor.ts
@@ -16,7 +16,7 @@ export class BearerTokenInterceptor implements HttpInterceptor {
   constructor(private readonly authService: AuthService) {}
 
   private retry = 0;
-  private readonly maxRetryCount = 6;
+  private readonly maxRetryCount = 20;
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     const headers: Record<string, string> = {
@@ -33,7 +33,7 @@ export class BearerTokenInterceptor implements HttpInterceptor {
     if (tokens !== null && this.retry < this.maxRetryCount) {
       return next.handle(req).pipe(
         catchError((error: HttpErrorResponse) => {
-          if (error.status === 401 && !req.url.includes("/refresh")) {
+          if (error.status === 401) {
             this.retry++;
             return this.handle401(request, next);
           }
