a523fab41aedbd3a11da2eb7afed430bf67e0b57
gregor <e@tokarev.work>
2023-04-24 13:26:49 +0300

    CU-85zrxer46 add onboarding 0 step

--- src/app/office/onboarding/onboarding.module.ts
+++ src/app/office/onboarding/onboarding.module.ts
@@ -5,9 +5,12 @@ import { CommonModule } from "@angular/common";
 import { OnboardingComponent } from "./onboarding/onboarding.component";
 import { OnboardingStageZeroComponent } from "@office/onboarding/stage-zero/onboarding-stage-zero.component";
 import { OnboardingRoutingModule } from "@office/onboarding/onboarding-routing.module";
+import { CoreModule } from "@core/core.module";
+import { ReactiveFormsModule } from "@angular/forms";
+import { UiModule } from "@ui/ui.module";
 
 @NgModule({
   declarations: [OnboardingComponent, OnboardingStageZeroComponent],
-  imports: [CommonModule, OnboardingRoutingModule],
+  imports: [CommonModule, OnboardingRoutingModule, CoreModule, ReactiveFormsModule, UiModule],
 })
 export class OnboardingModule {}
