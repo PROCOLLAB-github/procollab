c4215312638fa260c890e0e33ff8e531880fac73
gregor <e@tokarev.work>
2023-05-05 07:30:47 +0300

    CU-85zrza998 done sidebar

--- src/app/office/shared/sidebar/sidebar.component.ts
+++ src/app/office/shared/sidebar/sidebar.component.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+/** @format */
+
+import { Component, Input, OnInit } from "@angular/core";
+import { AuthService } from "@auth/services";
+import { NotificationService } from "@services/notification.service";
+import { InviteService } from "@services/invite.service";
+import { Invite } from "@models/invite.model";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-sidebar',
-  templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  selector: "app-sidebar",
+  templateUrl: "./sidebar.component.html",
+  styleUrls: ["./sidebar.component.scss"],
 })
 export class SidebarComponent implements OnInit {
+  constructor(
+    public readonly authService: AuthService,
+    private readonly notificationService: NotificationService,
+    private readonly inviteService: InviteService,
+    private readonly router: Router
+  ) {}
 
-  constructor() { }
+  @Input() invites: Invite[] = [];
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  showBall = this.notificationService.hasNotifications;
+
+  showNotifications = false;
+  get hasInvites(): boolean {
+    return !!this.invites.filter(invite => invite.isAccepted === null).length;
   }
 
+  onClickOutside() {
+    this.showNotifications = false;
+  }
+
+  onRejectInvite(inviteId: number): void {
+    this.inviteService.rejectInvite(inviteId).subscribe(() => {
+      const index = this.invites.findIndex(invite => invite.id === inviteId);
+      this.invites.splice(index, 1);
+
+      this.showNotifications = false;
+    });
+  }
+
+  onAcceptInvite(inviteId: number): void {
+    this.inviteService.acceptInvite(inviteId).subscribe(() => {
+      const index = this.invites.findIndex(invite => invite.id === inviteId);
+      const invite = JSON.parse(JSON.stringify(this.invites[index]));
+      this.invites.splice(index, 1);
+
+      this.showNotifications = false;
+      this.router
+        .navigateByUrl(`/office/projects/${invite.project.id}`)
+        .then(() => console.debug("Route changed from HeaderComponent"));
+    });
+  }
 }
