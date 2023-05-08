d5b478769a11b3f24e27f53af6a44e35341e4b81
gregor <e@tokarev.work>
2023-05-08 10:46:27 +0300

    CU-85zt0e6wy done

---
+++ src/app/core/pipes/years-from-birthday.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { YearsFromBirthdayPipe } from './years-from-birthday.pipe';
+
+describe('YearsFromBirthdayPipe', () => {
+  it('create an instance', () => {
+    const pipe = new YearsFromBirthdayPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
