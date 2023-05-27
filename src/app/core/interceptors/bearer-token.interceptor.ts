d6881842b4d0d0e7989c3f17b41ae1ec98f5c818
gregor <e@tokarev.work>
2023-05-27 16:27:14 +0300

    fix refresh interceptor

--- src/app/core/interceptors/bearer-token.interceptor.ts
+++ src/app/core/interceptors/bearer-token.interceptor.ts
@@ -8,15 +8,15 @@ import {
   HttpInterceptor,
   HttpRequest,
 } from "@angular/common/http";
-import { catchError, Observable, switchMap, throwError } from "rxjs";
+import { BehaviorSubject, catchError, filter, Observable, switchMap, take, throwError } from "rxjs";
 import { AuthService } from "@auth/services";
 
 @Injectable()
 export class BearerTokenInterceptor implements HttpInterceptor {
   constructor(private readonly authService: AuthService) {}
 
-  private retry = 0;
-  private readonly maxRetryCount = 20;
+  private isRefreshing = false;
+  private refreshTokenSubject = new BehaviorSubject<any>(null);
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     const headers: Record<string, string> = {
@@ -30,47 +30,72 @@ export class BearerTokenInterceptor implements HttpInterceptor {
     }
 
     const req = request.clone({ setHeaders: headers });
-    if (tokens !== null && this.retry < this.maxRetryCount) {
-      return next.handle(req).pipe(
-        catchError((error: HttpErrorResponse) => {
-          if (error.status === 401) {
-            this.retry++;
-            return this.handle401(request, next);
-          }
 
-          return throwError(() => error);
-        })
-      );
+    if (tokens !== null) {
+      return this.handleRequestWithTokens(req, next);
     } else {
       return next.handle(request);
     }
+  }
+
+  private handleRequestWithTokens(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    return next.handle(request).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 401) {
+          return this.handle401(request, next);
+        }
+
+        return throwError(() => error);
+      })
+    );
   }
 
   private handle401(
     request: HttpRequest<unknown>,
     next: HttpHandler
   ): Observable<HttpEvent<unknown>> {
-    return this.authService.refreshTokens().pipe(
-      switchMap(res => {
-        this.authService.memTokens(res);
-        const headers: Record<string, string> = {
-          Accept: "application/json",
-        };
+    if (!this.isRefreshing) {
+      this.isRefreshing = true;
+      this.refreshTokenSubject.next(null);
 
-        const tokens = this.authService.getTokens();
+      return this.authService.refreshTokens().pipe(
+        switchMap(res => {
+          this.isRefreshing = false;
+          this.refreshTokenSubject.next(res.access);
 
-        if (tokens) {
-          // eslint-disable-next-line
-          headers["Authorization"] = `Bearer ${tokens.access}`;
-        }
+          this.authService.memTokens(res);
+          const headers: Record<string, string> = {
+            Accept: "application/json",
+          };
 
-        this.retry = 0;
-        return next.handle(
-          request.clone({
-            setHeaders: headers,
-          })
-        );
-      })
+          const tokens = this.authService.getTokens();
+
+          if (tokens) {
+            headers["Authorization"] = `Bearer ${tokens.access}`;
+          }
+
+          return next.handle(
+            request.clone({
+              setHeaders: headers,
+            })
+          );
+        }),
+        catchError(err => {
+          this.isRefreshing = false;
+
+          this.authService.clearTokens();
+          return throwError(() => err);
+        })
+      );
+    }
+
+    return this.refreshTokenSubject.pipe(
+      filter(t => t !== null),
+      take(1),
+      switchMap(t => next.handle(request.clone({ setHeaders: { Authorization: `Bearer ${t}` } })))
     );
   }
 }
