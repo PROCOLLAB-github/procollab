04682db5fdd09d853b88f0e3b93d49ab8a16937b
gregor <e@tokarev.work>
2023-07-24 15:19:15 +0300

    done

--- src/app/office/program/detail/main/main.component.ts
+++ src/app/office/program/detail/main/main.component.ts
@@ -3,11 +3,10 @@
 import { Component, OnDestroy, OnInit } from "@angular/core";
 import { ProgramService } from "@office/program/services/program.service";
 import { ActivatedRoute } from "@angular/router";
-import { concatMap, map, Observable, of, Subscription, tap } from "rxjs";
+import { concatMap, noop, of, Subscription, tap } from "rxjs";
 import { Program } from "@office/program/models/program.model";
 import { ProgramNewsService } from "@office/program/services/program-news.service";
 import { ProjectNews, ProjectNewsRes } from "@office/projects/models/project-news.model";
-import { AngularNgNewOptionsSchema } from "@angular/cli/lib/config/workspace-schema";
 
 @Component({
   selector: "app-main",
@@ -39,6 +38,17 @@ export class ProgramDetailMainComponent implements OnInit, OnDestroy {
       .subscribe(news => {
         if (news.results?.length) {
           this.news = news.results;
+
+          setTimeout(() => {
+            const observer = new IntersectionObserver(this.onNewsInVew.bind(this), {
+              root: document.querySelector(".office__body"),
+              rootMargin: "0px 0px 0px 0px",
+              threshold: 0,
+            });
+            document.querySelectorAll(".news__item").forEach(e => {
+              observer.observe(e);
+            });
+          });
         }
       });
 
@@ -55,4 +65,26 @@ export class ProgramDetailMainComponent implements OnInit, OnDestroy {
   // program$?: Observable<Program> = this.route.parent?.data.pipe(map(r => r["data"]));
 
   readFullDescription = false;
+
+  onNewsInVew(entries: IntersectionObserverEntry[]): void {
+    const ids = entries.map(e => {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      return e.target.dataset.id;
+    });
+
+    this.programNewsService.readNews(this.route.snapshot.params["programId"], ids).subscribe(noop);
+  }
+
+  onLike(newsId: number) {
+    const item = this.news.find(n => n.id === newsId);
+    if (!item) return;
+
+    this.programNewsService
+      .toggleLike(this.route.snapshot.params["programId"], newsId, !item.isUserLiked)
+      .subscribe(() => {
+        item.likesCount = item.isUserLiked ? item.likesCount - 1 : item.likesCount + 1;
+        item.isUserLiked = !item.isUserLiked;
+      });
+  }
 }
--- src/app/office/program/services/program-news.service.ts
+++ src/app/office/program/services/program-news.service.ts
@@ -2,7 +2,7 @@
 
 import { Injectable } from "@angular/core";
 import { ApiService } from "@core/services";
-import { Observable } from "rxjs";
+import { forkJoin, Observable } from "rxjs";
 import { ProjectNewsRes } from "@office/projects/models/project-news.model";
 
 @Injectable({
@@ -13,5 +13,19 @@ export class ProgramNewsService {
 
   fetchNews(programId: number): Observable<ProjectNewsRes> {
     return this.apiService.get(`/programs/${programId}/news/`);
+  }
+
+  readNews(projectId: string, newsIds: number[]): Observable<void[]> {
+    return forkJoin(
+      newsIds.map(id =>
+        this.apiService.post<void>(`/programs/${projectId}/news/${id}/set_viewed/`, {})
+      )
+    );
+  }
+
+  toggleLike(projectId: string, newsId: number, state: boolean): Observable<void> {
+    return this.apiService.post(`/programs/${projectId}/news/${newsId}/set_liked/`, {
+      is_liked: state,
+    });
   }
 }
