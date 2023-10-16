a2575b262faba9f9a97c00d023ff8a94f2e2d37b
cloudsurf5 <da.drozdov101@gmail.com>
2023-10-16 15:13:09 +0800

    keep data between onboarding steps

---
+++ src/app/office/onboarding/services/onboarding.service.ts
@@ -0,0 +1,42 @@
+/** @format */
+
+import { Injectable } from "@angular/core";
+import { User } from "@auth/models/user.model";
+import { AuthService } from "@auth/services";
+import { BehaviorSubject, take } from "rxjs";
+
+@Injectable({
+  providedIn: "root",
+})
+export class OnboardingService {
+  constructor(private authService: AuthService) {
+    this.authService.profile.pipe(take(1)).subscribe(p => {
+      this._formValue$.next({
+        avatar: p.avatar,
+        city: p.city,
+        organization: p.organization,
+        speciality: p.speciality,
+        keySkills: p.keySkills,
+        userType: p.userType,
+      });
+
+      this._currentStage$.next(p.onboardingStage as number);
+    });
+  }
+
+  private _formValue$ = new BehaviorSubject<Partial<User>>({});
+  formValue$ = this._formValue$.asObservable();
+
+  private _currentStage$ = new BehaviorSubject<number | null>(0);
+  currentStage$ = this._currentStage$.asObservable();
+
+  setFormValue(updates: Partial<User>): void {
+    this.formValue$.pipe(take(1)).subscribe(fv => {
+      this._formValue$.next({ ...fv, ...updates });
+    });
+  }
+
+  setStep(step: number | null): void {
+    this._currentStage$.next(step);
+  }
+}
--- src/app/office/onboarding/stage-two/stage-two.component.ts
+++ src/app/office/onboarding/stage-two/stage-two.component.ts
@@ -2,8 +2,9 @@
 
 import { Component, OnDestroy, OnInit } from "@angular/core";
 import { AuthService } from "@auth/services";
-import { concatMap, Subscription } from "rxjs";
+import { concatMap, Subscription, take } from "rxjs";
 import { Router } from "@angular/router";
+import { OnboardingService } from "../services/onboarding.service";
 
 @Component({
   selector: "app-stage-two",
@@ -11,20 +12,32 @@ import { Router } from "@angular/router";
   styleUrls: ["./stage-two.component.scss"],
 })
 export class OnboardingStageTwoComponent implements OnInit, OnDestroy {
-  constructor(private readonly authService: AuthService, private readonly router: Router) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly onboardingService: OnboardingService,
+    private readonly router: Router
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    const formValue$ = this.onboardingService.formValue$.pipe(take(1)).subscribe(fv => {
+      this.userRole = fv.userType ? fv.userType : -1;
+    });
+
+    this.subscriptions$.push(formValue$);
+  }
 
   ngOnDestroy(): void {
     this.subscriptions$.forEach($ => $.unsubscribe());
   }
 
-  userRole = -1;
+  userRole!: number;
   stageTouched = false;
+  stageSubmitting = false;
   subscriptions$: Subscription[] = [];
 
   onSetRole(role: number) {
     this.userRole = role;
+    this.onboardingService.setFormValue({ userType: role });
   }
 
   onSubmit() {
@@ -32,6 +45,8 @@ export class OnboardingStageTwoComponent implements OnInit, OnDestroy {
       this.stageTouched = true;
       return;
     }
+
+    this.stageSubmitting = true;
 
     this.authService
       .saveProfile({ userType: this.userRole })
