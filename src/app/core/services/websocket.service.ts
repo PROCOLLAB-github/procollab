4330a6e2bd879b59251f394db722bbf797626946
gregor <e@tokarev.work>
2023-10-23 10:58:47 +0300

    Fix content

--- src/app/core/services/websocket.service.ts
+++ src/app/core/services/websocket.service.ts
@@ -51,7 +51,7 @@ export class WebsocketService {
     return this.messages$.asObservable().pipe(
       map(message => JSON.parse(message.data)),
       filter(message => message.type === type),
-      map(message => camelcaseKeys(message, { deep: true }))
+      map(message => camelcaseKeys(message.content, { deep: true }))
     );
   }
 
