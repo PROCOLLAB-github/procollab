5d954008d216cffa10e0e2fd279d77352c680f3b
cloudsurf5 <da.drozdov101@gmail.com>
2023-10-29 19:46:55 +0800

    fixed mobile version a bit

--- src/app/utils/responsive.ts
+++ src/app/utils/responsive.ts
@@ -2,3 +2,8 @@
 
 export const containerSm = 680;
 export const containerMd = 1280;
+
+export const calcAppHeight = (): void => {
+  const doc = document.documentElement;
+  doc.style.setProperty("--app-height", `${window.innerHeight}px`);
+};
