fd1589cd857aa28022be1fb248e43675159e3624
gregor <e@tokarev.work>
2023-11-15 18:08:36 +0300

    Add chat list updates

--- src/app/office/chat/chat.component.ts
+++ src/app/office/chat/chat.component.ts
@@ -1,39 +1,43 @@
 /** @format */
 
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { NavService } from "@services/nav.service";
 import { ActivatedRoute, Router } from "@angular/router";
-import { combineLatest, map, Observable } from "rxjs";
+import { BehaviorSubject, combineLatest, map, Observable, Subscription } from "rxjs";
 import { ChatListItem } from "@office/chat/models/chat-item.model";
 import { AuthService } from "@auth/services";
+import { ChatService } from "@services/chat.service";
 
 @Component({
   selector: "app-chat",
   templateUrl: "./chat.component.html",
   styleUrls: ["./chat.component.scss"],
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent implements OnInit, OnDestroy {
   constructor(
     private readonly navService: NavService,
     private readonly route: ActivatedRoute,
     private readonly router: Router,
-    private readonly authService: AuthService
+    private readonly authService: AuthService,
+    private readonly chatService: ChatService
   ) {}
+
+  chatsData = new BehaviorSubject<ChatListItem[]>([]);
 
   chats: Observable<ChatListItem[]> = combineLatest([
     this.authService.profile,
-    this.route.data.pipe<ChatListItem[]>(map(r => r["data"])),
+    this.chatsData,
   ]).pipe(
     map(([profile, chats]) =>
       chats.map(chat => ({
         ...chat,
-        unRead: profile.id !== chat.lastMessage.author.id && !chat.lastMessage.isRead,
+        unread: profile.id !== chat.lastMessage.author.id && !chat.lastMessage.isRead,
       }))
     ),
     map(chats =>
       chats.sort((prev, next) => {
-        if (prev.unRead && !next.unRead) return -1;
-        else if (!prev.unRead && next.unRead) return 1;
+        if (prev.unread && !next.unread) return -1;
+        else if (!prev.unread && next.unread) return 1;
         else return 0;
       })
     ),
@@ -42,6 +46,7 @@ export class ChatComponent implements OnInit {
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore
         delete chat.unread;
+
         return chat;
       })
     )
@@ -49,6 +54,25 @@ export class ChatComponent implements OnInit {
 
   ngOnInit(): void {
     this.navService.setNavTitle("Чат");
+
+    const messageSub$ = this.chatService.onMessage().subscribe(result => {
+      const newChatsData: ChatListItem[] = JSON.parse(JSON.stringify(this.chatsData.value));
+      const chatIdx = newChatsData.findIndex(c => c.id === result.chatId);
+
+      newChatsData.splice(chatIdx, 1, { ...newChatsData[chatIdx], lastMessage: result.message });
+
+      this.chatsData.next(newChatsData);
+    });
+    this.subscriptions$.push(messageSub$);
+
+    this.route.data.pipe<ChatListItem[]>(map(r => r["data"])).subscribe(chats => {
+      this.chatsData.next(chats);
+    });
+  }
+
+  subscriptions$: Subscription[] = [];
+  ngOnDestroy(): void {
+    this.subscriptions$.forEach($ => $.unsubscribe());
   }
 
   onGotoChat(id: string | number) {
