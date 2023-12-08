1f70b1e69bf6a0d23a4582e7efb9195195d8e967
cloudsurf5 <da.drozdov101@gmail.com>
2023-12-08 12:08:55 +0800

    changed button and added animation

--- src/app/office/members/filters/members-filters.component.ts
+++ src/app/office/members/filters/members-filters.component.ts
@@ -2,13 +2,13 @@
 
 import { CommonModule } from "@angular/common";
 import { ChangeDetectionStrategy, Component, Output, EventEmitter } from "@angular/core";
-import { ButtonComponent, IconComponent } from "@ui/components";
+import { IconComponent } from "@ui/components";
 import { RangeInputComponent } from "@ui/components/range-input/range-input.component";
 
 @Component({
   selector: "app-members-filters",
   standalone: true,
-  imports: [CommonModule, RangeInputComponent, ButtonComponent, IconComponent],
+  imports: [CommonModule, RangeInputComponent, IconComponent],
   templateUrl: "./members-filters.component.html",
   styleUrl: "./members-filters.component.scss",
   changeDetection: ChangeDetectionStrategy.OnPush,
