19873324d85d6a3419686bc853fb9be4065cd39f
gregor <e@tokarev.work>
2023-12-12 11:31:02 +0300

    fix tests

--- src/app/office/program/models/program.model.ts
+++ src/app/office/program/models/program.model.ts
@@ -26,7 +26,7 @@ export class Program {
       name: "",
       description: "",
       city: "",
-      imageAddress: "",
+      imageAddress: "https://google.com",
       presentationAddress: "",
       links: [],
       coverImageAddress: "",
