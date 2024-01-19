8a108a82e912f83bdaf375ef358d8f9a8343ee78
gregor <e@tokarev.work>
2024-01-19 09:44:22 +0300

    Rebuild program

--- src/app/office/program/detail/projects/projects.component.ts
+++ src/app/office/program/detail/projects/projects.component.ts
@@ -1,25 +1,37 @@
 /** @format */
 
-import { Component, OnInit } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 import { ActivatedRoute, RouterLink } from "@angular/router";
 import { map, Observable } from "rxjs";
 import { Project } from "@models/project.model";
 import { Program } from "@office/program/models/program.model";
 import { ProjectCardComponent } from "@office/shared/project-card/project-card.component";
 import { ProgramHeadComponent } from "../../shared/program-head/program-head.component";
-import { NgIf, NgFor, AsyncPipe } from "@angular/common";
+import { NgIf, NgFor, AsyncPipe, JsonPipe } from "@angular/common";
 
 @Component({
   selector: "app-projects",
   templateUrl: "./projects.component.html",
   styleUrl: "./projects.component.scss",
   standalone: true,
-  imports: [NgIf, ProgramHeadComponent, NgFor, RouterLink, ProjectCardComponent, AsyncPipe],
+  imports: [
+    NgIf,
+    ProgramHeadComponent,
+    NgFor,
+    RouterLink,
+    ProjectCardComponent,
+    AsyncPipe,
+    JsonPipe,
+  ],
 })
 export class ProgramProjectsComponent implements OnInit {
   constructor(private readonly route: ActivatedRoute) {}
 
-  projects$: Observable<Project[]> = this.route.data.pipe(map(r => r["data"]));
+  projects$: Observable<Project[]> = this.route.data.pipe(
+    map(r => r["data"]),
+    map(r => r["results"])
+  );
+
   program$?: Observable<Program> = this.route.parent?.data.pipe(map(r => r["data"]));
 
   ngOnInit(): void {}
