3a1985e565ed04a6229582cf2187d6982fc3a172
gregor <e@tokarev.work>
2024-01-25 09:34:15 +0300

    Add page `/feed`

---
+++ src/app/office/feed/feed.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-feed',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './feed.component.html',
+  styleUrl: './feed.component.scss'
+})
+export class FeedComponent {
+
+}
