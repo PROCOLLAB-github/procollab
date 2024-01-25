085233be2cc67e11cf630f12aa41a82062073f69
gregor <e@tokarev.work>
2024-01-25 09:34:15 +0300

    Add page `/feed`

---
+++ src/app/office/feed/feed.routes.ts
@@ -0,0 +1,11 @@
+/** @format */
+
+import { Routes } from "@angular/router";
+import { FeedComponent } from "@office/feed/feed.component";
+
+export const FEED_ROUTES: Routes = [
+  {
+    path: "",
+    component: FeedComponent,
+  },
+];
