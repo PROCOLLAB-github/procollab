41b6f416d9670e4f511bfccd178b412b74b9ca9c
gregor <e@tokarev.work>
2024-02-15 14:20:25 +0300

    replace change-detection with signals

--- src/app/office/feed/feed.component.ts
+++ src/app/office/feed/feed.component.ts
@@ -1,6 +1,6 @@
 /** @format */
 
-import { Component, inject, OnDestroy, OnInit } from "@angular/core";
+import { Component, inject, OnDestroy, OnInit, signal } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { OpenVacancyComponent } from "@office/feed/shared/open-vacancy/open-vacancy.component";
 import { NewProjectComponent } from "@office/feed/shared/new-project/new-project.component";
@@ -9,6 +9,7 @@ import { ActivatedRoute } from "@angular/router";
 import { FeedItem } from "@office/feed/models/feed-item.model";
 import { map, Subscription } from "rxjs";
 import { NewsCardComponent } from "@office/shared/news-card/news-card.component";
+import { ProjectNews } from "@office/projects/models/project-news.model";
 
 @Component({
   selector: "app-feed",
@@ -28,13 +29,13 @@ export class FeedComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     const routeData$ = this.route.data.pipe(map(r => r["data"])).subscribe((feed: FeedItem[]) => {
-      this.feedItems = feed;
+      this.feedItems.set(feed);
     });
-    this.subscriptions$.push(routeData$);
+    this.subscriptions$().push(routeData$);
   }
 
   ngOnDestroy() {
-    this.subscriptions$.forEach($ => $.unsubscribe());
+    this.subscriptions$().forEach($ => $.unsubscribe());
   }
 
   feedItems: FeedItem[] = [];
