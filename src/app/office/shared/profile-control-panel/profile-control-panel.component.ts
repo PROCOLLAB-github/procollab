9ff641ee090ca9d6852c90df0255b1d7f987f1c4
cloudsurf5 <da.drozdov101@gmail.com>
2024-02-21 01:24:02 +0800

    updated sidebar

---
+++ src/app/office/shared/profile-control-panel/profile-control-panel.component.ts
@@ -0,0 +1,52 @@
+/** @format */
+
+import { CommonModule } from "@angular/common";
+import { ChangeDetectionStrategy, Component, Input, Output, EventEmitter } from "@angular/core";
+import { InviteManageCardComponent } from "../invite-manage-card/invite-manage-card.component";
+import { ProfileInfoComponent } from "@ui/components/profile-info/profile-info.component";
+import { ClickOutsideModule } from "ng-click-outside";
+import { User } from "@auth/models/user.model";
+import { Invite } from "@office/models/invite.model";
+import { IconComponent } from "@ui/components";
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: "app-profile-control-panel",
+  standalone: true,
+  imports: [
+    CommonModule,
+    InviteManageCardComponent,
+    ProfileInfoComponent,
+    ClickOutsideModule,
+    IconComponent,
+    RouterLink,
+  ],
+  templateUrl: "./profile-control-panel.component.html",
+  styleUrl: "./profile-control-panel.component.scss",
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ProfileControlPanelComponent {
+  @Input({ required: true }) user!: User | null;
+
+  @Input({ required: true }) invites!: Invite[];
+
+  @Input({ required: true }) hasNotifications = false;
+
+  @Input({ required: true }) hasUnreads = false;
+
+  @Output() acceptInvite = new EventEmitter<number>();
+
+  @Output() rejectInvite = new EventEmitter<number>();
+
+  @Output() logout = new EventEmitter();
+
+  get hasInvites(): boolean {
+    return !!this.invites.filter(invite => invite.isAccepted === null).length;
+  }
+
+  showNotifications = false;
+
+  onClickOutside() {
+    this.showNotifications = false;
+  }
+}
