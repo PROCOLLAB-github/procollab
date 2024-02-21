a80552114f66c8165f80bf91944299a94606b4e1
gregor <e@tokarev.work>
2024-02-21 10:44:47 +0300

    update feedService

--- src/app/office/feed/services/feed.service.ts
+++ src/app/office/feed/services/feed.service.ts
@@ -13,22 +13,30 @@ import { HttpParams } from "@angular/common/http";
 export class FeedService {
   constructor(private readonly apiService: ApiService) {}
 
+  readonly splitSymbol = "|";
+
   getFeed(
     offset: number,
     limit: number,
     type: FeedItemType[] | FeedItemType
   ): Observable<ApiPagination<FeedItem>> {
+    let reqType: string;
+
+    if (type.length === 0) {
+      reqType = ["vacancy", "news", "project"].join(this.splitSymbol);
+    } else if (Array.isArray(type)) {
+      reqType = type.join(this.splitSymbol);
+    } else {
+      reqType = type;
+    }
+
     return this.apiService.get(
       "/feed/",
       new HttpParams({
         fromObject: {
           limit,
           offset,
-          type: Array.isArray(type)
-            ? type.length === 0
-              ? ["vacancy", "news", "project"].join("|")
-              : type.join("|")
-            : type,
+          type: reqType,
         },
       })
     );
