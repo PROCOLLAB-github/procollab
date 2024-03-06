6f37f9a459c9b3dae1dee832dce2ebd9e42a040d
gregor <e@tokarev.work>
2024-03-06 16:36:14 +0300

    move main app to projects folder

---
+++ projects/social_platform/src/app/auth/login/login.component.ts
@@ -0,0 +1,88 @@
+/** @format */
+
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from "@angular/forms";
+import { AuthService } from "../services";
+import { ErrorMessage } from "@error/models/error-message";
+import { ValidationService } from "projects/social_platform/src/app/core/services";
+import { ActivatedRoute, Router, RouterLink } from "@angular/router";
+import { ControlErrorPipe } from "@core/pipes/control-error.pipe";
+import { IconComponent, ButtonComponent, InputComponent } from "@ui/components";
+
+@Component({
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrl: "./login.component.scss",
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    RouterLink,
+    InputComponent,
+    ButtonComponent,
+    IconComponent,
+    ControlErrorPipe,
+  ],
+})
+export class LoginComponent implements OnInit {
+  constructor(
+    private readonly fb: FormBuilder,
+    private readonly authService: AuthService,
+    private readonly route: ActivatedRoute,
+    private readonly router: Router,
+    private readonly validationService: ValidationService,
+    private readonly cdref: ChangeDetectorRef
+  ) {
+    this.loginForm = this.fb.group({
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", [Validators.required]],
+    });
+  }
+
+  loginForm: FormGroup;
+  loginIsSubmitting = false;
+
+  errorWrongAuth = false;
+
+  errorMessage = ErrorMessage;
+
+  ngOnInit(): void {
+    this.authService.clearTokens();
+  }
+
+  onSubmit() {
+    const redirectType = this.route.snapshot.queryParams["redirect"];
+
+    if (!this.validationService.getFormValidation(this.loginForm) || this.loginIsSubmitting) {
+      return;
+    }
+
+    this.loginIsSubmitting = true;
+
+    this.authService.login(this.loginForm.value).subscribe({
+      next: res => {
+        this.authService.memTokens(res);
+        this.loginIsSubmitting = false;
+
+        this.cdref.detectChanges();
+
+        if (!redirectType)
+          this.router
+            .navigateByUrl("/office")
+            .then(() => console.debug("Route changed from LoginComponent"));
+        else if (redirectType === "program")
+          this.router
+            .navigateByUrl("/office/program/list")
+            .then(() => console.debug("Route changed from LoginComponent"));
+      },
+      error: error => {
+        if (error.status === 401) {
+          this.errorWrongAuth = true;
+        }
+
+        this.loginIsSubmitting = false;
+        this.cdref.detectChanges();
+      },
+    });
+  }
+}
--- src/app/office/program/models/project-rating-criterion-output.ts
+++
@@ -1,6 +0,0 @@
-/** @format */
-
-export interface ProjectRatingCriterionOutput {
-  criterionId: number;
-  value: unknown;
-}
