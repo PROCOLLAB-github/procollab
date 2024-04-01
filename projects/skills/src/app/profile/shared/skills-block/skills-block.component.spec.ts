1301aaff1f4c8e8bf3252fe7043b993ffc772113
gregor <e@tokarev.work>
2024-04-01 10:41:32 +0300

    add skills block

---
+++ projects/skills/src/app/profile/shared/skills-block/skills-block.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SkillsBlockComponent } from './skills-block.component';
+
+describe('SkillsBlockComponent', () => {
+  let component: SkillsBlockComponent;
+  let fixture: ComponentFixture<SkillsBlockComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [SkillsBlockComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(SkillsBlockComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- projects/skills/src/app/profile/skills/skills.component.ts
+++ projects/skills/src/app/profile/skills/skills.component.ts
@@ -4,11 +4,12 @@ import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { InfoBlockComponent } from "../shared/info-block/info-block.component";
 import { MonthBlockComponent } from "../shared/month-block/month-block.component";
+import { SkillsBlockComponent } from "../shared/skills-block/skills-block.component";
 
 @Component({
   selector: "app-skills",
   standalone: true,
-  imports: [CommonModule, InfoBlockComponent, MonthBlockComponent],
+  imports: [CommonModule, InfoBlockComponent, MonthBlockComponent, SkillsBlockComponent],
   templateUrl: "./skills.component.html",
   styleUrl: "./skills.component.scss",
 })
