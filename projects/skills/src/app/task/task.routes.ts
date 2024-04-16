d15e7f858a158f0c749b963f71eeaf076405ffbb
gregor <e@tokarev.work>
2024-04-16 15:34:26 +0300

    Add task complete screen

--- projects/skills/src/app/task/task.routes.ts
+++ projects/skills/src/app/task/task.routes.ts
@@ -2,11 +2,18 @@
 import { Routes } from "@angular/router";
 import { TaskComponent } from "./task/task.component";
 import { SubtaskComponent } from "./subtask/subtask.component";
+import { TaskCompleteComponent } from "./complete/complete.component";
 
 export const TASK_ROUTES: Routes = [
   {
     path: ":taskId",
     component: TaskComponent,
-    children: [{ path: ":subTaskId", component: SubtaskComponent }],
+    children: [
+      {
+        path: "results",
+        component: TaskCompleteComponent,
+      },
+      { path: ":subTaskId", component: SubtaskComponent },
+    ],
   },
 ];
