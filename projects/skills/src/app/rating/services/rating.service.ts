700c1e7e5c12bbad3d4839fd685e304aff1854f5
gregor <e@tokarev.work>
2024-05-16 08:19:39 +0300

    Fix rating screen

--- projects/skills/src/app/rating/services/rating.service.ts
+++ projects/skills/src/app/rating/services/rating.service.ts
@@ -3,6 +3,8 @@
 import { inject, Injectable } from "@angular/core";
 import { ApiService } from "@corelib";
 import { GeneralRating } from "../../../models/rating.model";
+import { map } from "rxjs";
+import { ApiPagination } from "../../../models/api-pagination.model";
 
 @Injectable({
   providedIn: "root",
@@ -11,6 +13,8 @@ export class RatingService {
   apiService = inject(ApiService);
 
   getGeneralRating() {
-    return this.apiService.get<GeneralRating[]>("/progress/user-rating/");
+    return this.apiService
+      .get<ApiPagination<GeneralRating>>("/progress/user-rating/")
+      .pipe(map(res => res.results));
   }
 }
