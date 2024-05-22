f91e8ca27e800a7f8535b3e2c318d06413fa64c5
Artem <artem.bqpower@gmail.com>
2024-05-22 14:20:52 +0300

    add close modal functionnality

--- projects/skills/src/app/profile/home/subscription-plans/subscription-plans.component.ts
+++ projects/skills/src/app/profile/home/subscription-plans/subscription-plans.component.ts
@@ -4,11 +4,12 @@ import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { IconComponent } from "@uilib";
 import { ButtonComponent } from "@ui/components";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: "app-subscription-plans",
   standalone: true,
-  imports: [CommonModule, IconComponent, ButtonComponent],
+  imports: [CommonModule, IconComponent, ButtonComponent, RouterLink],
   templateUrl: "./subscription-plans.component.html",
   styleUrl: "./subscription-plans.component.scss",
 })
