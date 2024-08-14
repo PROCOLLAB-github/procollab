ee769debbca623915c099536a0d6ea4b5ac621f4
Artem <artem.bqpower@gmail.com>
2024-08-14 16:10:54 +0300

    run prettier

--- projects/social_platform/src/app/office/projects/detail/info/info.component.ts
+++ projects/social_platform/src/app/office/projects/detail/info/info.component.ts
@@ -78,7 +78,7 @@ export class ProjectInfoComponent implements OnInit, AfterViewInit, OnDestroy {
     private readonly subscriptionService: SubscriptionService,
     private readonly projectService: ProjectService,
     private readonly cdRef: ChangeDetectorRef
-  ) { }
+  ) {}
 
   project$?: Observable<Project> = this.route.parent?.data.pipe(map(r => r["data"][0]));
   projSubscribers$?: Observable<User[]> = this.route.parent?.data.pipe(map(r => r["data"][1]));
@@ -181,7 +181,7 @@ export class ProjectInfoComponent implements OnInit, AfterViewInit, OnDestroy {
 
     this.projectNewsService
       .delete(this.route.snapshot.params["projectId"], newsId)
-      .subscribe(() => { });
+      .subscribe(() => {});
   }
 
   onLike(newsId: number) {
