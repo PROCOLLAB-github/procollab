897a3285f4df40c3b5c51d202d7f1c8256212467
Artem <artem.bqpower@gmail.com>
2024-10-11 18:57:15 +0300

    change routing for skills

--- projects/skills/src/app/profile/skills-rating/skills-rating.component.ts
+++ projects/skills/src/app/profile/skills-rating/skills-rating.component.ts
@@ -1,10 +1,12 @@
 /** @format */
 
-import { Component, inject, OnInit } from "@angular/core";
+import { Component, inject, OnInit, signal } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { PersonalRatingCardComponent } from "../shared/personal-rating-card/personal-rating-card.component";
 import { Profile } from "projects/skills/src/models/profile.model";
 import { ProfileService } from "../services/profile.service";
+import { SkillService } from "../../skills/services/skill.service";
+import { Skill } from "projects/skills/src/models/skill.model";
 
 @Component({
   selector: "app-skills-rating",
@@ -15,6 +17,8 @@ import { ProfileService } from "../services/profile.service";
 })
 export class ProfileSkillsRatingComponent implements OnInit {
   profileService = inject(ProfileService);
+  skillService = inject(SkillService);
+
   skillsList: Profile["skills"] = [];
 
   ngOnInit(): void {
