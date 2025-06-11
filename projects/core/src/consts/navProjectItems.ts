73431ec9acbebaa9a779fb08ef322f3fa317acfd
Awakich <artem.bqpower@gmail.com>
2025-06-11 20:35:31 +0300

    add shared consts

---
+++ projects/core/src/consts/navProjectItems.ts
@@ -0,0 +1,29 @@
+/** @format */
+
+export const navItems = [
+  {
+    step: "main",
+    src: "/assets/images/projects/edit/main.svg",
+    label: "Основные данные",
+  },
+  {
+    step: "contacts",
+    src: "/assets/images/projects/edit/contacts.svg",
+    label: "Контакты",
+  },
+  {
+    step: "achievements",
+    src: "/assets/images/projects/edit/achievements.svg",
+    label: "Достижения",
+  },
+  {
+    step: "team",
+    src: "/assets/images/projects/edit/members.svg",
+    label: "Участники",
+  },
+  {
+    step: "vacancies",
+    src: "/assets/images/projects/edit/vacancies.svg",
+    label: "Вакансии",
+  },
+];
