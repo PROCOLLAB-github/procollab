d52b477cc4384cb2a25ec26eee8f7048f78f2650
Awakich <artem.bqpower@gmail.com>
2025-08-19 16:19:16 +0300

    add team members block in edit project

---
+++ projects/social_platform/src/app/office/shared/collaborator-card/collaborator-card.component.spec.ts
@@ -0,0 +1,26 @@
+/** @format */
+
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+
+import { InviteCardComponent } from "./collaborator-card.component";
+
+describe("VacancyCardComponent", () => {
+  let component: InviteCardComponent;
+  let fixture: ComponentFixture<InviteCardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [InviteCardComponent],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(InviteCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it("should create", () => {
+    expect(component).toBeTruthy();
+  });
+});
