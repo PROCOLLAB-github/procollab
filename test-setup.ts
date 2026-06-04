28255a136ae5ce9fe960d5cfc0c782c82033254e
Awakich <artem.bqpower@gmail.com>
2026-06-04 21:07:58 +0300

    test(sp): настройка Vitest для social_platform

--- test-setup.ts
+++ test-setup.ts
@@ -2,4 +2,16 @@
 
 import { setupTestBed } from "@analogjs/vitest-angular/setup-testbed";
 
+if (typeof IntersectionObserver === "undefined") {
+  (globalThis as any).IntersectionObserver = class {
+    observe() {}
+    unobserve() {}
+    disconnect() {}
+  };
+}
+
+if (!Element.prototype.scrollTo) {
+  (Element.prototype as any).scrollTo = function () {};
+}
+
 setupTestBed();
