9384fa295cf556f5ec4d4b74ff64e8bb4979f573
Awakich <artem.bqpower@gmail.com>
2026-06-05 17:45:56 +0300

    add test setup

--- test-setup.ts
+++ test-setup.ts
@@ -14,4 +14,18 @@ if (!Element.prototype.scrollTo) {
   (Element.prototype as any).scrollTo = function () {};
 }
 
+if (!URL.createObjectURL) {
+  let counter = 0;
+  const objectURLs = new Map<string, string>();
+  (URL as any).createObjectURL = function (blob: Blob): string {
+    const id = `blob:${++counter}`;
+    const url = `blob:${id}`;
+    objectURLs.set(url, blob as any);
+    return url;
+  };
+  (URL as any).revokeObjectURL = function (url: string): void {
+    objectURLs.delete(url);
+  };
+}
+
 setupTestBed();
